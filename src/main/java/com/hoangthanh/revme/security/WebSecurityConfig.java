1ac8c7e2bccf102ce799aeedc143a9218bdb31f8
DHoangThanh1306 <dhoangthanh1306@gmail.com>
2024-11-27 08:04:26 +0700

    Add: Login Admin

--- src/main/java/com/hoangthanh/revme/security/WebSecurityConfig.java
+++ src/main/java/com/hoangthanh/revme/security/WebSecurityConfig.java
@@ -90,6 +90,7 @@ public class WebSecurityConfig { // extends WebSecurityConfigurerAdapter {
 				.authorizeHttpRequests(
 						auth -> auth
 						.requestMatchers("/api/auth/**").permitAll()
+						.requestMatchers("/api/login/**").permitAll()
 						.anyRequest().authenticated());
 
 		http.authenticationProvider(authenticationProvider());
